657d43534ef3af1569f1b6d3015f81d9467557ca
hpnrep6 <57055412+hpnrep6@users.noreply.github.com>
2020-03-20 00:59:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const {trigger,token} = require('./config.json');
+const {trigger, token} = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const ytdl = require('ytdl-core');
-const queue = new Map();
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -14,13 +12,29 @@ function isNum(num) {
     return !isNaN(num);
 }
 
+
 client.on('message', async message => {
-    const args = message.content.split(" ");
-    // spam ----------------------
-    let indexOfSpace = message.content.indexOf(" ");
-    if(message.content.startsWith("!spam") && indexOfSpace != -1 && isNum(message.content.substring(5,indexOfSpace))) {
+    let args = message.content.split(" ");
+    let messageContent = message.content;
+    let indexOfSpace = messageContent.indexOf(" ");
+
+    if(messageContent.startsWith(trigger + "spammember") && indexOfSpace != -1 && isNum(messageContent.substring(11,indexOfSpace))) {
+        spamMember();
+    }    
+    else if(messageContent.startsWith(trigger + "spamrand") && indexOfSpace != -1 && message.member.hasPermission('MANAGE_ROLES')) {
+        spamRandom();
+    }
+    else if(messageContent.startsWith(trigger + "spameveryone") && args.length > 1 && message.member.hasPermission('MANAGE_ROLES')) {
+        spamEveryone();
+    }
+    else if(messageContent.startsWith(trigger + "spammessage") && indexOfSpace != -1 && isNum(messageContent.substring(12, indexOfSpace)) && message.member.hasPermission('MANAGE_ROLES')) {
+        spamMessage();
+    }
+
+    // spam functions
+    function spamMember() {
         if(message.member.hasPermission('MANAGE_ROLES')){
-            let messageRepeat = parseInt(message.content.substring(5, indexOfSpace));
+            let messageRepeat = parseInt(messageContent.substring(11, indexOfSpace));
             try{
                 let userId = message.mentions.users.first().id;
                 for(i = 0; i < messageRepeat % 250; i++) {
@@ -29,8 +43,8 @@ client.on('message', async message => {
             }catch(e){}
         }
         else{
-            if(message.content.startsWith("!spam") && indexOfSpace != -1 && isNum(message.content.substring(5,indexOfSpace))) {
-                let messageRepeat = parseInt(message.content.substring(5, indexOfSpace));
+            if(messageContent.startsWith(trigger + "spam") && indexOfSpace != -1 && isNum(messageContent.substring(5,indexOfSpace))) {
+                let messageRepeat = parseInt(messageContent.substring(5, indexOfSpace));
                 try{
                     let userId = message.mentions.users.first().id;
                     for(i = 0; i < messageRepeat % 250; i++) {
@@ -39,8 +53,8 @@ client.on('message', async message => {
                 }catch(e){}
             }
         }
-    }    
-    else if(message.content.startsWith("!spamrand") && indexOfSpace != -1 && message.member.hasPermission('MANAGE_ROLES')) {
+    }
+    function spamRandom() {
         try{
             let userId = message.mentions.users.first().id;
             for(i = 0; i < (Math.random()*92412131) % 100; i++) {
@@ -48,12 +62,18 @@ client.on('message', async message => {
             }
         }catch(e){}
     }
-    else if(message.content.startsWith("!spameveryone") && args.length > 1 && message.member.hasPermission('MANAGE_ROLES')) {
+    function spamEveryone() {
         try{
             for(i = 0; i < parseInt(args[1]) % 251; i++) {
                 message.channel.send("@everyone");
             }
         }catch(e){}    
     }
-    // ---------------------
+    function spamMessage() {
+        try{
+            for(i = 0; i < parseInt(messageContent.substring(12, indexOfSpace)) % 500; i++) {
+                message.channel.send(messageContent.substring(indexOfSpace + 1));
+            }
+        } catch (e) {}
+    }
 });
